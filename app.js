e0a92f9232eed580978f3c7ab83b9d5cb3a7ca6f
Xin <1243236087@qq.com>
2021-03-23 17:36:21 +0800

    add express server

---
+++ app.js
@@ -0,0 +1,10 @@
+// 引用express
+const express = require('express');
+// 创建express 实例
+const app = express();
+// 配置 静态资源文件 
+app.use(express.static(__dirname + '/build'))
+// 监听端口号
+app.listen(6789,()=>{
+  console.log('服务已启动')
+})
--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,57 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const {
+  override,
+  addLessLoader,
+  fixBabelImports,
+  // useEslintRc,
+  addWebpackPlugin,
+  addWebpackAlias
+} = require("customize-cra");
+const path = require("path");
+// const envs = require("./env");
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const chalk = require('chalk');
+
+const resolve = dir => path.join(__dirname, ".", dir);
+const rewiredMap = () => config => {
+  config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false;
+  return config;
+};
+// const webpack = require("webpack");
 
 module.exports = override(
-   fixBabelImports('import', {
-    libraryName: 'antd',
-    libraryDirectory: 'es',
+  addLessLoader({
+    importedLoaders: 1,
+    javascriptEnabled: true,
+    modifyVars: { 
+      "@primary-color": "#ED8A11",
+      "@font-size-base": "12px",
+      "@border-radius-base": "2px",
+    }
+  }),
+  fixBabelImports("import", {
+    libraryName: "antd",
+    libraryDirectory: "es",
     style: true,
   }),
-  addLessLoader({
-    javascriptEnabled: true,
+  // useEslintRc(".eslintrc"),
+  // close source map
+  rewiredMap(),
+  addWebpackAlias({
+    ["@"]: resolve("src"),
+    '@components': path.resolve(__dirname, 'src/components'),
   }),
- );
+  // addWebpackPlugin(
+  //   new webpack.DefinePlugin(envs.get(process.argv[2].split("=")[1]))
+  // ),
+  // yarn build progress
+  addWebpackPlugin(new ProgressBarPlugin({
+    complete: "█",
+    format: `${chalk.green('Building')} [ ${chalk.green(':bar')} ] ':msg:' ${chalk.bold('(:percent)')}`,
+    clear: true
+  })),
+  addWebpackPlugin(new CompressionWebpackPlugin({
+    test: /\.js$|\.css$/,
+    threshold: 1024,
+  }))
+);
